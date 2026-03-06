c4570beda073e7edd3e88f0b758ed35d839ea174
Bunny099 <jayeshkhuman121@gmail.com>
2026-03-07 00:23:02 +0530

    feat:webhook payload

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello prsageian")
+import express from "express";
+
+import SmeeClient from "smee-client";
+
+const app = express();
+app.use(express.json());
+const smee = new SmeeClient({
+  source: "",
+  target: "http://localhost:3000/events",
+  logger: console,
+});
+
+await smee.start();
+
+app.post("/events",async (req, res) => {
+  try {
+    const data = req.body;
+    const dataStr = JSON.stringify(data);
+    console.log(dataStr)
+    res.status(200).json({ dataStr, message: "payload received" });
+  } catch (e) {
+    res.status(500).json({ message: "server error!" });
+  }
+});
+app.listen(3000, () => console.log("Port is running on 3000!"));
