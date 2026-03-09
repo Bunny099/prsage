deba7bbe3af3ff4218167ba04e1d3ee12f875655
Bunny099 <jayeshkhuman121@gmail.com>
2026-03-09 23:59:12 +0530

    feat:post comment feature added

--- src/github/pullRequest.ts
+++ src/github/pullRequest.ts
@@ -1,10 +1,18 @@
 import type { Octokit } from "octokit";
-
 interface GetFilesInput {
-  octo: Octokit;
-  owner: string;
-  repo: string;
-  pull_number: number;
+  octo: Octokit,
+  owner: string,
+  repo: string,
+  pull_number: number,
+}
+
+interface PostCommentInput{
+  
+   octo: Octokit,
+  owner: string,
+  repo: string,
+  pull_number: number,
+  body: string
 }
 
 export const getPrFiles = async ({
@@ -24,3 +32,14 @@ export const getPrFiles = async ({
     throw new Error("Error while fetching pr files!");
   }
 };
+
+
+export const postComment = async({octo,owner,repo,pull_number,body}:PostCommentInput)=>{
+  try{
+  
+  const response =await octo.rest.issues.createComment({owner,repo,body,issue_number:pull_number})
+  return response;
+  }catch(e){
+    throw new Error("Error while posting a comment")
+  }
+}
