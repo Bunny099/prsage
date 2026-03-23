8dfa3d7fc6945b46c73bbc6b0e4fd9f1df5a3edd
Bunny099 <jayeshkhuman121@gmail.com>
2026-03-23 10:21:34 +0530

    feat:update jwttoken and types

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,18 +1,23 @@
 import fs from "fs";
 import jwt from "jsonwebtoken";
+import "dotenv/config.js"
 
+let APP_ID = process.env.APP_ID;
+if(!APP_ID){
+  throw new Error("APP is missing!")
+}
 export const jwtToken = async () => {
   const privateKey = fs.readFileSync("./file/private-key.pem", "utf-8");
   const now = Math.floor(Date.now() / 1000);
   const payload = {
-    iss: process.env.APP_ID!,
+    iss: APP_ID,
     iat: now,
     exp: now + 9 * 60,
   };
   const signOptions = {
     algorithm: "RS256",
   };
-  //@ts-ignore
+  // @ts-ignore
   const token = await jwt.sign(payload, privateKey, signOptions);
   return token;
 };
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,15 @@
+export interface ExtractedFileInput {
+  file: string;
+  patch: string;
+}
 
-export interface ExtractedFileInput{
-    file:string,
-    patch:string
+export interface ExtractedCommentInput {
+  data: [{ id: number; body: string }];
+}
+
+export interface ReviewPullRequestInput{
+    id:number,
+    repo:string,
+    owner:string,
+    pull_number:number
 }
