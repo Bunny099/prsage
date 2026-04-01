f66acc69b8d21605e66ad70537d7661fc4f5460e
Bunny099 <jayeshkhuman121@gmail.com>
2026-04-01 22:44:58 +0530

    feat:dev env in server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import "dotenv/config.js"
 import app from "./app.js"
 
-app.listen(3000)
+const PORT = Number(process.env.PORT) || 3000;
+let dev = process.env.NODE_ENV === "development";
+app.listen(PORT,()=>{
+    console.log(`Server is running on port: ${PORT}`);
+    console.log(`Environment: ${dev? "development" :"production"}`)
+})
